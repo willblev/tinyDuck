7b09da28adc8d4df3fe3d714d6eba2dea108f0a2
Will <willblev@gmail.com>
2017-04-21 22:02:49 +0200

    Create example_0.ino

---
+++ example_scripts/example_0.ino
@@ -0,0 +1,26 @@
+/*
+SOURCE: Adapted from DigiStump example that can be found here:
+https://github.com/digistump/DigistumpArduino/blob/master/digistump-avr/libraries/DigisparkKeyboard/examples/Keyboard/Keyboard.ino
+TARGET OS: Windows, Mac, Linux
+NOTES: Types 'Hello, World!'; repeats every 5 seconds. 
+#include "DigiKeyboard.h"
+
+void setup() {
+  // don't need to set anything up to use DigiKeyboard
+}
+
+
+void loop() {
+  // this is generally not necessary but with some older systems it seems to
+  // prevent missing the first character after a delay:
+  DigiKeyboard.sendKeyStroke(0);
+  
+  // Type out this string letter by letter on the computer (assumes US-style
+  // keyboard)
+  DigiKeyboard.println("Hello, World!");
+  
+  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
+  // if doing keyboard stuff because it keeps talking to the computer to make
+  // sure the computer knows the keyboard is alive and connected
+  DigiKeyboard.delay(5000);
+}
